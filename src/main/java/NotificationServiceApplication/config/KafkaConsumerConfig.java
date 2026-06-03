189f0c4c56ffd8d8e26ef6e87821628d36d99e3e
Sujay Awale <sujayawale125@gmail.com>
2026-06-04 01:00:49 +0530

    running code

--- src/main/java/NotificationServiceApplication/config/KafkaConsumerConfig.java
+++ src/main/java/NotificationServiceApplication/config/KafkaConsumerConfig.java
@@ -13,11 +13,12 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 public class KafkaConsumerConfig {
 
 	@Bean
-	public ConsumerFactory<String, Object> consumerFactory(){
+	public ConsumerFactory<String, String> consumerFactory(){
 		Map<String, Object> props =new HashMap<String, Object>();
 		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		props.put(ConsumerConfig.GROUP_ID_CONFIG, "notification-group");
 		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-		return new DefaultKafkaConsumerFactory<String, Object>(props);
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		return new DefaultKafkaConsumerFactory<String, String>(props);
 	}
 }
--- src/main/java/NotificationServiceApplication/consumer/NotificationConsumer.java
+++ src/main/java/NotificationServiceApplication/consumer/NotificationConsumer.java
@@ -3,6 +3,8 @@ package NotificationServiceApplication.consumer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import NotificationServiceApplication.dto.DeliveryEvent;
 import NotificationServiceApplication.dto.OrderEvent;
 import NotificationServiceApplication.dto.PaymentEvent;
@@ -15,19 +17,39 @@ public class NotificationConsumer {
 
 	private final NotificationService notificationService;
 	
+	private final ObjectMapper mapper =new ObjectMapper();
+	
 	@KafkaListener(topics ="order-created-topic", groupId = "notification-group")
-	public void orderCreated(OrderEvent event) {
-		notificationService.orderCreated(event);
+	public void orderCreated(String message) throws Exception{
+		OrderEvent event =
+                mapper.readValue(
+                        message,
+                        OrderEvent.class
+                );
+
+        notificationService.orderCreated(event);
 	}
 	
 	@KafkaListener(topics ="payment-completed-topic", groupId = "notification-group")
-	public void paymentCompleted(PaymentEvent event) {
-		notificationService.paymentCompleted(event);
+	public void paymentCompleted(String message) throws Exception{
+		PaymentEvent event =
+	            mapper.readValue(
+	                    message,
+	                    PaymentEvent.class
+	            );
+
+	    notificationService.paymentCompleted(event);
 	}
 	
 	@KafkaListener(topics ="delivery-created-topic", groupId = "notification-group")
-	public void deliveryAssigned(DeliveryEvent event) {
-		notificationService.deliveryAssigned(event);
+	public void deliveryAssigned(String message) throws Exception{
+		 DeliveryEvent event =
+		            mapper.readValue(
+		                    message,
+		                    DeliveryEvent.class
+		            );
+
+		    notificationService.deliveryAssigned(event);
 	}
 	
 }
