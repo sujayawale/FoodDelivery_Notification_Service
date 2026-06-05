125751d57ebdcaf37f7926f36263472e71b63e08
Sujay Awale <sujayawale125@gmail.com>
2026-06-05 22:17:13 +0530

    running code

--- src/main/java/NotificationServiceApplication/NotificationServiceApplication.java
+++ src/main/java/NotificationServiceApplication/NotificationServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NotificationServiceApplication.class, args); 
+		SpringApplication.run(NotificationServiceApplication.class, args);   
 	}
 
 }
